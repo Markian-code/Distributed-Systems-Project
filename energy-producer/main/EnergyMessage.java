33c5de7375f0edb6a52d88493c19f3f8d84ac58b
Kovalko <wi23b189@technikum-wien.at>
2025-04-06 11:54:23 +0200

    Initial commit: add energy-producer

---
+++ energy-producer/main/EnergyMessage.java
@@ -0,0 +1,19 @@
+package at.technikum.producer;
+
+import java.time.LocalDateTime;
+
+public class EnergyMessage {
+    public String type;
+    public String association;
+    public double kwh;
+    public LocalDateTime datetime;
+
+    public EnergyMessage(String type, String association, double kwh, LocalDateTime datetime) {
+        this.type = type;
+        this.association = association;
+        this.kwh = kwh;
+        this.datetime = datetime;
+    }
+
+    public EnergyMessage() {}
+}
